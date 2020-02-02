ef2196305e3a1298a1e318b915658018bc1cdf6a
retxxxirt <retxxxirt@gmail.com>
2020-02-02 13:03:46 +0800

    Added missed django management files to wheel package.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='django-fixtures-rx',
     packages=['django_fixtures'],
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Advanced django fixtures.',
     long_description=open('README.md').read(),
@@ -13,5 +13,6 @@ setup(
     author_email='retxxirt@gmail.com',
     url='https://github.com/retxxxirt/django-fixtures',
     keywords=['django', 'django fixtures', 'django-fixtures', 'fixtures', 'tests'],
-    install_requires=['wheel']
+    install_requires=['wheel'],
+    package_data = {'django_fixtures': ['management/commands/*.py']}
 )
